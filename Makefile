02c9eb1a7184e3a0800e1b976e5e07d2476163a7
Joaquín Arias <joaquinariasgomezcal@gmail.com>
2023-05-01 14:55:31 +0200

    We have leds running

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ GO=GOFLAGS=-mod=vendor go
 .PHONY: run
 run:
 	$(GO) run src/main.go
+
+.PHONY: deps
+deps:
+	$(GO) mod tidy
--- src/api/endpoints.go
+++ src/api/endpoints.go
@@ -3,9 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"goledserver/src/ledstrip"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the homepage!")
 	fmt.Println("Endpoint Hit: homePage")
+	ledstrip.StartLedStrip()
 }
