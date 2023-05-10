d6a46ef5ed3167b71ffd7058af272db88dc3c4ed
joaquinariasgomez <joaquinariasgomezcal@gmail.com>
2023-05-10 09:37:07 +0200

    WIP 2

--- src/ledstrip/execution.go
+++ src/ledstrip/execution.go
@@ -22,19 +22,11 @@ func (e *execution) StartTask(a Action) {
 		// If there are no jobs, it will create and launch one.
 		// Otherwise, it will stop the current job and launch another.
 		if e.currentJob.ID == "" {
-			newJob := new(Job)
-			newJob.Create(a)
-
-			e.currentJob = *newJob
-			e.currentJob.Start()
+			e.createAndStartJob(a)
 		} else {
-			if e.currentJob.status != "stopped" {
-				e.currentJob.Stop()
-			}
 			newJob := new(Job)
 			newJob.Create(a)
-			// Wait for the current execution to finish
-			e.currentJob.wg.Wait()
+			e.stopCurrentJobAndWaitForFinish()
 
 			e.currentJob = *newJob
 			e.currentJob.Start()
@@ -46,37 +38,22 @@ func (e *execution) StopTask() {
 
 }
 
-/*func (e *execution) createJob(a Action) {
-	var newJob Job
-	newJob.ID = uuid.NewV4().String()
-	newJob.Action = a
-	newJob.wg = sync.WaitGroup{}
-	newJob.wg.Add(1)
+func (e *execution) createAndStartJob(a Action) {
+	newJob := new(Job)
+	newJob.Create(a)
 
-	e.currentJob = newJob
+	e.currentJob = *newJob
 	e.currentJob.Start()
-}*/
+}
 
-/*func (e *execution) createAndLaunchJob(a Action) {
-	var newJob Job
-	newJob.ID = uuid.NewV4().String()
-	newJob.Action = a
-	newJob.wg = sync.WaitGroup{}
-	newJob.wg.Add(1)
-
-	e.currentJob = newJob
-	e.currentJob.Start()
-}*/
-
-/*func (e *execution) stopAndLaunchJob(a Action) {
+func (e *execution) stopCurrentJobAndWaitForFinish() {
 	// Stop current execution if it isn't already
 	if e.currentJob.status != "stopped" {
 		e.currentJob.Stop()
 	}
 	// Wait for the current execution to finish
 	e.currentJob.wg.Wait()
-	e.createAndLaunchJob(a)
-}*/
+}
 
 var executionInstance *execution
 
